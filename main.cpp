d143ee2244714d228721d614bcadf62f0cf8c296
JoseGi24 <josegiachino@yahoo.com.ar>
2024-07-08 22:53:55 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,6 +3,18 @@
 #include "mbed.h"
 #include "arm_book_lib.h"
 
+// Declaracion e inicilizacion de objetos globales publicos //
+
+DigitalIn botonAvanzar(D2);
+DigitalIn botonAtras(D3);
+DigitalIn botonDerecha(D4);
+DigitalIn botonIzquierda(D5);
+DigitalIn botonEnter(D6);
+DigitalIn echoUltrason(D7);
+
+
+
+
 int main()
 
 
