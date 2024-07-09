27e5d3a6d28aac17a51ee987a1bc9806840ac4e4
JoseGi24 <josegiachino@yahoo.com.ar>
2024-07-09 20:42:14 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,10 +11,50 @@ DigitalIn botonDerecha(D4);
 DigitalIn botonIzquierda(D5);
 DigitalIn botonEnter(D6);
 DigitalIn echoUltrason(D7);
+DigitalIn encoderDerecha(D8);
+DigitalIn encoderIzquierda(D9);
 
+DigitalOut motorH1(D10);
+DigitalOut motorH2(D11);
+DigitalOut triggerUltras(D12);
+DigitalOut ledVerde(D13);
+DigitalOut ledRojo(D14);
 
+//    Declaracion e inicializacion de variables globales publicas//
 
+//  Prototipos de funciones  //
+void inputInit();
+void outputInit();
 
 int main()
+{
+
+}
+
+// Implementacion de funciones publicas //
+
+void inputInit() {
+  botonAvanzar.mode(PullDown);
+  botonAtras.mode(PullDown);
+  botonDerecha.mode(PullDown);
+  botonIzquierda.mode(PullDown);
+  botonEnter.mode(PullDown);
+  echoUltrason.mode(PullDown);
+  encoderDerecha.mode(PullDown);
+  encoderIzquierda.mode(PullDown);
+  }
+
+void outputinit() {
+  motorH1 = OFF;
+  motorH1 = OFF;
+  triggerUltras= OFF;
+  ledVerde = OFF;
+  ledRojo = OFF;
+  }
+
+
+
+
+
 
 
